0460f970927cedfd0a1bbc26a6c56a148a94cf6b
Aymeric Secret <aymericsecret@regularswitch.com>
2018-10-24 16:30:14 -0200

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Layout from '../components/layout';
+
+export default class About extends Component {
+  render() {
+    return (
+      <Layout>
+        <h1>About Us</h1>
+        <p>
+        Tumeric wayfarers hella portland meh mlkshk cardigan kale chips. Hell of organic mumblecore marfa green juice man braid leggings 8-bit mustache disrupt locavore +1 gastropub. Kale chips intelligentsia hot chicken, fam drinking vinegar selfies roof party hammock tbh microdosing typewriter. Glossier man braid keytar meh chillwave wayfarers meggings.
+        </p>
+      </Layout>
+    )
+  }
+}
